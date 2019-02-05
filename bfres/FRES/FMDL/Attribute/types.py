a726bf17bbd17a708498b2311424213015ecf796
Rena Kunisaki <hyperhacker@gmail.com>
2019-02-04 19:01:03 -0500

    use ARM variant half-float format

--- bfres/FRES/FMDL/Attribute/types.py
+++ bfres/FRES/FMDL/Attribute/types.py
@@ -12,6 +12,23 @@ def unpack10bit(val):
     return res
 
 
+def unpackArmHalfFloat(val):
+    """Unpack 16-bit half-precision float.
+
+    Uses ARM alternate format which does not encode Inf/NaN.
+    """
+    if type(val) in (list, tuple):
+        return list(map(unpacArmkHalfFloat, val))
+    frac = (val & 0x3FF) / 0x3FF
+    exp  = (val >> 10) & 0x1F
+    sign = -1 if (val & 0x8000) else 1
+    if exp == 0:
+        return sign * (2 ** -14) * frac
+    else:
+        return sign * (2 ** (exp-15)) * (1+frac)
+
+
+
 typeRanges = { # name: (min, max)
     'b': (       -128,        127),
     'B': (          0,        255),
@@ -92,14 +109,16 @@ attrFmts = {
         'name':  'u8[4]',
     },
     0x1205: {
-        'fmt':   '2e',   # half float
+        'fmt':   '2H',   # half float
         'ctype': 'float',
         'name':  'half[2]',
+        'func':  unpackArmHalfFloat,
     },
     0x1505: {
-        'fmt':   '4e',
+        'fmt':   '4H',
         'ctype': 'float',
         'name':  'half[4]',
+        'func':  unpackArmHalfFloat,
     },
     0x1705: {
         'fmt':   '2f',
